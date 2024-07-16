4e74b8693b0826792d782c7827a99bd553afee53
최홍석 <che0618@naver.com>
2024-07-16 12:38:11 +0900

    Feat: 액자 회전 컴포넌트 구현
    Former-commit-id: 8bd6866c5804ebfd023fb7657d738cd6ff6568c1

--- frontend/src/components/Model/ImgRotationBtn.jsx
+++ frontend/src/components/Model/ImgRotationBtn.jsx
@@ -10,7 +10,6 @@ const ImgRotationBtn = () => {
   const texture = useLoader(TextureLoader, '/models/texture_1.jpg');
   const updateRotation = useFrameStore((state) => state.updateRotation);
 
-  // console.log(materials['frameMaterial.001']);
   materials['frameMaterial.001'].map = texture;
   texture.center.set(0.5, 0.5);
   // // 왼쪽으로 90도 돌리기
