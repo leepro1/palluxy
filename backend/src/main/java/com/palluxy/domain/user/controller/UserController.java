90210b916abf1f9dd231eefaa42d956825485b38
이희주 <leemoon63@gmail.com>
2024-07-17 15:48:13 +0900

    Feat: 비밀번호 찾기 로직 구현
    Former-commit-id: 9c04dea6c2af2c2ccbec94e883853630bd275bb3

--- backend/src/main/java/com/palluxy/domain/user/controller/UserController.java
+++ backend/src/main/java/com/palluxy/domain/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.palluxy.domain.user.controller;
 
+import com.palluxy.domain.user.dto.request.UserResetPasswordRequest;
 import com.palluxy.domain.user.dto.request.UserSignupRequest;
 import com.palluxy.domain.user.exception.SignupFormatException;
 import com.palluxy.domain.user.service.UserService;
@@ -27,9 +28,26 @@ public class UserController {
         return CommonResponse.created("회원가입 성공");
     }
 
-    @GetMapping("/{nickname}")
-    public CommonResponse<?> deleteMember(@PathVariable("nickname") String nickname) {
+    @GetMapping("/check-nickname/{nickname}")
+    @ResponseStatus(HttpStatus.OK)
+    public CommonResponse<?> checkNickname(@PathVariable("nickname") String nickname) {
         userService.duplicateNickname(nickname);
         return CommonResponse.ok("닉네임 사용 가능");
     }
+
+    @GetMapping("/reset-password")
+    @ResponseStatus(HttpStatus.OK)
+    public CommonResponse<?> resetPassword(@RequestParam("code") String code) {
+        System.out.println("code>>>>>>>>>>>>>>>>>>>>>"+code);
+        userService.verifyResetPasswordCode(code);
+        return CommonResponse.ok("비밀번호 code 인증 성공");
+    }
+
+    @PatchMapping("/reset-password")
+    @ResponseStatus(HttpStatus.OK)
+    public CommonResponse<?> resetPassword(@Valid @RequestBody UserResetPasswordRequest request, BindingResult bindingResult) {
+        userService.resetPassword(request);
+        return CommonResponse.ok("비밀번호 변경 성공");
+    }
+
 }
--- backend/src/main/java/com/palluxy/domain/user/service/UserService.java
+++ backend/src/main/java/com/palluxy/domain/user/service/UserService.java
@@ -1,6 +1,8 @@
 package com.palluxy.domain.user.service;
 
+import com.palluxy.domain.user.dto.request.UserResetPasswordRequest;
 import com.palluxy.domain.user.dto.request.UserSignupRequest;
+import com.palluxy.domain.user.exception.BadResetPasswordCodeUserException;
 import com.palluxy.domain.user.exception.DuplicateUserException;
 
 public interface UserService {
@@ -8,4 +10,8 @@ public interface UserService {
     void signup(UserSignupRequest request) throws DuplicateUserException;
 
     void duplicateNickname(String nickname) throws DuplicateUserException;
+
+    void resetPassword(UserResetPasswordRequest request);
+
+    void verifyResetPasswordCode(String code) throws BadResetPasswordCodeUserException;
 }
