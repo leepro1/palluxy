ef026db65e6c881ea5e94d34f34127e914401975
최홍석 <che0618@naver.com>
2024-07-17 12:50:11 +0900

    Design: 메인페이지 디자인
    Former-commit-id: 1c4cba3e7ca2cf7d0e3c079d32bd014a0a22aded

--- frontend/src/pages/HomePage/index.jsx
+++ frontend/src/pages/HomePage/index.jsx
@@ -3,7 +3,35 @@ import ContentsLayout from '@layout/ContentsLayout';
 const HomePage = () => {
   return (
     <ContentsLayout>
-      <h1>Home 입니다</h1>
+      <div className="flex flex-col">
+        {/* slo */}
+        <div className="font-jamsilBold flex flex-col items-center gap-y-3 py-16 text-5xl text-white">
+          <h1>반려동물의 상실을 겪은 당신을 위한</h1>
+          <h1>PAL:Luxy</h1>
+        </div>
+
+        {/* preview */}
+        <div className="mx-5 my-10">
+          <div className="flex justify-between">
+            {/* 추억공간 */}
+            <div className="font-jamsilBold flex flex-col gap-y-5 text-white">
+              <h2 className="text-3xl">반려동물 추억공간</h2>
+              <div className="h-[300px] w-[500px] bg-black"></div>
+              <h3 className="font-jamsilRegular text-xl">
+                반려동물을 만나보세요
+              </h3>
+            </div>
+            {/* 치유모임 */}
+            <div className="font-jamsilBold flex flex-col gap-y-5 text-white">
+              <h2 className="text-3xl">치유모임</h2>
+              <div className="h-[300px] w-[500px] bg-black"></div>
+              <h3 className="font-jamsilRegular text-xl">
+                사람들을 만나보세요
+              </h3>
+            </div>
+          </div>
+        </div>
+      </div>
     </ContentsLayout>
   );
 };
