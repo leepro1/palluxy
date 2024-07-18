2fe39248d58adbc2545b80847bd799083d8ea770
박진형 <dyunames21@gmail.com>
2024-07-18 13:50:50 +0900

    Feat : 인덱스 번호 생성시 직접 선택
    Former-commit-id: 67a1d8521663c4879a7d6fb20cf91cef3e6a084b

--- backend/src/main/java/com/palluxy/global/error/CommonExceptionHandler.java
+++ backend/src/main/java/com/palluxy/global/error/CommonExceptionHandler.java
@@ -3,19 +3,18 @@ package com.palluxy.global.error;
 import com.palluxy.domain.user.exception.DuplicateUserException;
 import com.palluxy.global.common.CommonResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler({
-            DuplicateUserException.class,
-            RuntimeException.class
+        DuplicateUserException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResponse<?> handleDuplicateUserException(String msg) {
-        return CommonResponse.badRequest(msg);
+    public CommonResponse<?> handleDuplicateUserException(DuplicateUserException ex) {
+        return CommonResponse.badRequest(ex.getMessage());
     }
 }
