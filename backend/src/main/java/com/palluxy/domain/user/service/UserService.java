433be39e7d8c8569190a361099af996c38bd6c8d
이희주 <leemoon63@gmail.com>
2024-07-19 12:43:11 +0900

    Reapply "Feat: 유저 조회, 중복 검사 로직 추가"
    This reverts commit 1b75df48dde4df15174aba96d89ecdd0d6eaa247 [formerly f8f90bceea83f3652f58f3d2f4eec040b01d1a74].
    
    
    Former-commit-id: a76e42c8451d28e453b853f3089a9e8b5517afa8

--- backend/src/main/java/com/palluxy/domain/user/service/UserService.java
+++ backend/src/main/java/com/palluxy/domain/user/service/UserService.java
@@ -2,16 +2,30 @@ package com.palluxy.domain.user.service;
 
 import com.palluxy.domain.user.dto.request.UserResetPasswordRequest;
 import com.palluxy.domain.user.dto.request.UserSignupRequest;
+import com.palluxy.domain.user.dto.response.UserResponse;
 import com.palluxy.domain.user.exception.BadResetPasswordCodeUserException;
 import com.palluxy.domain.user.exception.DuplicateUserException;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserService {
 
-    void signup(UserSignupRequest request) throws DuplicateUserException;
+    void signup(UserSignupRequest request);
 
-    void duplicateNickname(String nickname) throws DuplicateUserException;
+    void duplicateEmail(String email);
+
+    void duplicateNickname(String nickname);
 
     void resetPassword(UserResetPasswordRequest request);
 
-    void verifyResetPasswordCode(String code) throws BadResetPasswordCodeUserException;
+    void verifyResetPasswordCode(String code);
+
+    List<UserResponse> getUsers();
+
+    UserResponse getUserById(Long id);
+
+    UserResponse getUserByEmail(String email);
+
+    UserResponse getUserByNickname(String nickname);
 }
--- backend/src/main/java/com/palluxy/global/error/CommonExceptionHandler.java
+++ backend/src/main/java/com/palluxy/global/error/CommonExceptionHandler.java
@@ -1,21 +1,19 @@
 package com.palluxy.global.error;
 
-import com.palluxy.domain.user.exception.DuplicateUserException;
 import com.palluxy.global.common.CommonResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class CommonExceptionHandler {
 
     @ExceptionHandler({
-            DuplicateUserException.class,
             RuntimeException.class
     })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public CommonResponse<?> handleDuplicateUserException(String msg) {
-        return CommonResponse.badRequest(msg);
+    public CommonResponse<?> handleRuntimeException(RuntimeException ex) {
+        return CommonResponse.badRequest(ex.getMessage());
     }
 }
