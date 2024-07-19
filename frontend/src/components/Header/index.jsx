a1695d2aa4642e463578819041aa45b3410cec49
차재훈 <wogns264@gmail.com>
2024-07-19 10:24:57 +0900

    Feat: rtc ui 구현 완
    Former-commit-id: cff7642e49cdd25e832b80d40c2a9a733db764c0

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -16,12 +16,12 @@ const Header = () => {
         </div>
         {/* nav item */}
         <div className="mx-7 grow">
-          <ul className="font-jamsilMedium flex gap-x-16 text-xl text-white">
+          <ul className="flex gap-x-16 font-jamsilMedium text-xl text-white">
             <li>
               <NavLink to={'/'}>공지사항</NavLink>
             </li>
             <li>
-              <NavLink to={'/'}>치유모임</NavLink>
+              <NavLink to={'/healingmeeting'}>치유모임</NavLink>
             </li>
             <li>
               <NavLink to={'/memoryspace'}>추억공간</NavLink>
@@ -32,7 +32,7 @@ const Header = () => {
           </ul>
         </div>
         {/* 로그인 회원가입 버튼 */}
-        <div className="font-jamsilMedium flex gap-x-16 text-white">
+        <div className="flex gap-x-16 font-jamsilMedium text-white">
           <Button
             className="border-2 border-white"
             size={'sm'}
