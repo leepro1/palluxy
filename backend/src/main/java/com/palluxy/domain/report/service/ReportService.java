c7f8bc6cc4fd90c51e0a111aeca22089d05328b7
leebokyeong <qhrud7007@naver.com>
2024-07-30 16:46:22 +0900

    Feat: 유효성 검사 기능 추가
    Former-commit-id: 2899d596ec95d76c1802d769e575153f99314dfe

--- backend/src/main/java/com/palluxy/domain/report/service/ReportService.java
+++ backend/src/main/java/com/palluxy/domain/report/service/ReportService.java
@@ -1,9 +1,17 @@
 package com.palluxy.domain.report.service;
 
+import com.palluxy.domain.memoryRoom.guestbook.entity.Comment;
+import com.palluxy.domain.memoryRoom.guestbook.repository.CommentRepository;
+import com.palluxy.domain.memoryRoom.room.entity.Room;
+import com.palluxy.domain.memoryRoom.room.repository.RoomRepository;
 import com.palluxy.domain.report.entity.GuestBookReport;
 import com.palluxy.domain.report.entity.RoomReport;
 import com.palluxy.domain.report.repository.GuestBookRepository;
 import com.palluxy.domain.report.repository.RoomReportRepository;
+import com.palluxy.domain.user.entity.User;
+import com.palluxy.domain.user.repository.UserRepository;
+import com.palluxy.global.common.error.NotFoundException;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +21,9 @@ public class ReportService {
 
   private final GuestBookRepository guestBookRepository;
   private final RoomReportRepository roomReportRepository;
+  private final UserRepository userRepository;
+  private final CommentRepository commentRepository;
+  private final RoomRepository roomRepository;
 
   public void createGuestBookReport(GuestBookReport guestBookReport) {
     isValidGuestBookReport(guestBookReport);
@@ -25,22 +36,35 @@ public class ReportService {
   }
 
   public void isValidGuestBookReport(GuestBookReport guestBookReport) {
-
+    isValidUser(guestBookReport.getReportTo());
+    isValidUser(guestBookReport.getReportFrom());
+    isValidComment(guestBookReport.getCommentId());
   }
 
   public void isValidRoomReport(RoomReport roomReport) {
-
+    isValidUser(roomReport.getReportTo());
+    isValidUser(roomReport.getReportFrom());
+    isValidRoom(roomReport.getRoomId());
   }
 
   public void isValidUser(Long userId) {
-
+    Optional<User> findUser = userRepository.findById(userId);
+    if (findUser.isPresent()) {
+      throw new NotFoundException("user");
+    }
   }
 
-  public void isValidComment(Long commentId, Long guestBookId) {
-
+  public void isValidComment(Long commentId) {
+    Optional<Comment> findComment = commentRepository.findById(commentId);
+    if (findComment.isPresent()) {
+      throw new NotFoundException("comment");
+    }
   }
 
   public void isValidRoom(Long roomId) {
-
+    Optional<Room> findRoom = roomRepository.findById(roomId);
+    if (findRoom.isPresent()) {
+      throw new NotFoundException("room");
+    }
   }
 }
