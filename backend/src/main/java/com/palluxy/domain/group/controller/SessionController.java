ee09ba8cfaa4cb3e99625932bc9aee7379a30eae
차재훈 <wogns264@gmail.com>
2024-08-01 15:11:06 +0900

    Feat: webRTC back과 연결 확인
    Former-commit-id: bfc7eb1f5d660443f5dda9db7b1ddb159a1d1f48

--- backend/src/main/java/com/palluxy/domain/group/controller/SessionController.java
+++ backend/src/main/java/com/palluxy/domain/group/controller/SessionController.java
@@ -18,7 +18,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequiredArgsConstructor
 public class
