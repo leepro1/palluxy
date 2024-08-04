24227b33c5a8425699a58d30a6e40265b74fcc7d
최홍석 <che0618@naver.com>
2024-08-04 15:19:38 +0900

    Feat: 로그인 라우터 가드 적용
    Former-commit-id: 27187797146be71aacef74dfa14f0bd2a5b6b404

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,3 +1,5 @@
+// gaurd
+import AuthGuard from './AuthGuard';
 // home
 import HomePage from '@pages/HomePage';
 import MainLayout from '@layout/MainLayout';
@@ -56,7 +58,14 @@ const routerInfo = [
         element: <NoticeDetail />,
       },
       { path: '/noticeboard/create', element: <CreateNotice /> },
-      { path: '/healingmeeting', element: <HealingMeetingPage /> },
+      {
+        path: '/healingmeeting',
+        element: (
+          <AuthGuard auth={true}>
+            <HealingMeetingPage />
+          </AuthGuard>
+        ),
+      },
       {
         path: '/meetingoverview',
         children: [
@@ -73,7 +82,11 @@ const routerInfo = [
       //   { path: '/signin', element: <SigninPage /> },
       {
         path: '/memoryspacecreate',
-        element: <MemorySpaceCreatePage />,
+        element: (
+          <AuthGuard auth={true}>
+            <MemorySpaceCreatePage />,
+          </AuthGuard>
+        ),
       },
       {
         path: '/memoryspace',
@@ -103,13 +116,32 @@ const routerInfo = [
       // { path: '/signin', element: <SigninPage /> },
       //   { path: '/community', element: <CommunityPage /> },
       { path: '/404', element: <NotFound /> },
-      { path: '/signin', element: <SignInPage /> },
-      { path: '/signup', element: <SignUpPage /> },
+      {
+        path: '/signin',
+        element: (
+          <AuthGuard auth={false}>
+            <SignInPage />
+          </AuthGuard>
+        ),
+      },
+      {
+        path: '/signup',
+        element: (
+          <AuthGuard auth={false}>
+            <SignUpPage />
+          </AuthGuard>
+        ),
+      },
       { path: '/find', element: <FindPasswordPage /> },
       { path: '/reset', element: <ResetPasswordPage /> },
       {
         path: '/mypage',
-        element: <MyPage />,
+        element: (
+          <AuthGuard auth={true}>
+            <MyPage />
+          </AuthGuard>
+        ),
+
         children: [
           // { index: true, element: <PersonalInfo /> },
           {
@@ -126,7 +158,14 @@ const routerInfo = [
           },
         ],
       },
-      { path: '/admin', element: <AdminPage /> },
+      {
+        path: '/admin',
+        element: (
+          <AuthGuard auth={true}>
+            <AdminPage />
+          </AuthGuard>
+        ),
+      },
     ],
   },
 ];
