52418603ffa73f9c84c120d8df127aee71dd6414
차재훈 <wogns264@gmail.com>
2024-08-05 10:16:45 +0900

    Chore: branch 수정용 백업
    Former-commit-id: c10827eef6f2cc1d4f43770d9dcd4fd0ef3fba99

--- frontend/src/pages/AdminPage/index.jsx
+++ frontend/src/pages/AdminPage/index.jsx
@@ -6,7 +6,11 @@ const AdminPage = () => {
   return (
     <ContentsLayout>
       <div>
-        <p className="text-4xl text-pal-lightwhite">어드민 페이지</p>
+        <p className="text-4xl text-pal-lightwhite">관리자 페이지</p>
+        <div className="flex justify-center">
+          <button className="mx-36 bg-pal-purple">치유모임</button>
+          <button className="mx-36">공지사항</button>
+        </div>
       </div>
     </ContentsLayout>
   );
