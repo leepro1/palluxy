cd167bd7ffe4c20acf95c687c93f2af58e59763f
박진형 <dyunames21@gmail.com>
2024-08-07 10:37:35 +0900

    Feat: responseTimeout 설정
    Former-commit-id: 9d65ae2b8ecb55e137087aac48ab7a998b429e2f

--- backend/src/main/java/com/palluxy/global/config/WebClientConfig.java
+++ backend/src/main/java/com/palluxy/global/config/WebClientConfig.java
@@ -1,14 +1,23 @@
 package com.palluxy.global.config;
 
+import io.netty.channel.ChannelOption;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
     public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+        HttpClient httpClient = HttpClient.create()
+            .responseTimeout(Duration.ofSeconds(60));
+
+        return WebClient.builder()
+            .clientConnector(new ReactorClientHttpConnector(httpClient));
     }
 }
