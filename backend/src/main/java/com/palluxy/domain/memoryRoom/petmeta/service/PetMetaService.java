2c6159831d69e3eb2320b79615b05a0d3a483929
박진형 <dyunames21@gmail.com>
2024-08-07 10:11:11 +0900

    Feat: django에서 s3이미지 url 받는걸로 수정
    Former-commit-id: 2fe0ed420b8516ccdb39f16af4f517f2e85557b4

--- backend/src/main/java/com/palluxy/domain/memoryRoom/petmeta/service/PetMetaService.java
+++ backend/src/main/java/com/palluxy/domain/memoryRoom/petmeta/service/PetMetaService.java
@@ -1,6 +1,7 @@
 package com.palluxy.domain.memoryRoom.petmeta.service;
 
 import com.palluxy.domain.memoryRoom.petmeta.dto.PetMetaDto;
+import com.palluxy.domain.memoryRoom.petmeta.dto.WebhookRequest;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
@@ -8,22 +9,32 @@ import reactor.core.publisher.Mono;
 import java.util.List;
 
 public interface PetMetaService {
-  PetMetaDto createPetMeta(PetMetaDto petMetaDto, Long roomId);
-  PetMetaDto getPetMetaById(Long petMetaId);
-  List<PetMetaDto> getPetMetaByRoomId(Long roomId);
-  PetMetaDto updatePetMeta(Long petMetaId, PetMetaDto petMetaDto);
 
-  PetMetaDto updatePetMetaPositionRotation(Long petMetaId, double positionX, double positionY, double positionZ, double rotationX, double rotationY, double rotationZ);
+    PetMetaDto createPetMeta(PetMetaDto petMetaDto, Long roomId);
 
-  PetMetaDto updatePetMetaPosition(Long petMetaId, double positionX, double positionY, double positionZ);
+    PetMetaDto getPetMetaById(Long petMetaId);
 
-  PetMetaDto updatePetMetaRotation(Long petMetaId, double rotationX, double rotationY, double rotationZ);
+    List<PetMetaDto> getPetMetaByRoomId(Long roomId);
 
-  void deletePetMeta(Long petMetaId);
+    PetMetaDto updatePetMeta(Long petMetaId, PetMetaDto petMetaDto);
 
-  Mono<String> uploadImageToDjangoAndProcess(Long roomId, MultipartFile file);
+    PetMetaDto updatePetMetaPositionRotation(Long petMetaId, double positionX, double positionY,
+        double positionZ, double rotationX, double rotationY, double rotationZ);
 
-  Mono<String> processWebhook(Long roomId, FilePart filePart);
+    PetMetaDto updatePetMetaPosition(Long petMetaId, double positionX, double positionY,
+        double positionZ);
 
-  Mono<String> handleObjFileUpload(Long roomId, FilePart filePart);
+    PetMetaDto updatePetMetaRotation(Long petMetaId, double rotationX, double rotationY,
+        double rotationZ);
+
+    void deletePetMeta(Long petMetaId);
+
+    Mono<String> uploadImageToDjangoAndProcess(Long roomId, MultipartFile file);
+
+    Mono<String> processWebhook(Long roomId, FilePart filePart);
+
+    Mono<String> handleObjFileUpload(Long roomId, FilePart filePart);
+
+    WebhookRequest handleWebhookData(Long roomId, String file); // 변경된 메서드
+
 }
