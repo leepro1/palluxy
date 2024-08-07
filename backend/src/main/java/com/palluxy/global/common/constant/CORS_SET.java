7f1dc88cfed29b3561db065555cfe5a78b0fb551
이희주 <leemoon63@gmail.com>
2024-08-07 23:19:13 +0900

    Fix: 시큐리티 test cors 설정
    Former-commit-id: 7bb32b88aa7cd6474853af72e3ea321112cca648

--- backend/src/main/java/com/palluxy/global/common/constant/CORS_SET.java
+++ backend/src/main/java/com/palluxy/global/common/constant/CORS_SET.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public final class CORS_SET {
 
-    public static final List<String> ALLOWED_ORIGINS = Collections.singletonList("https://i11a208.p.ssafy.io");
+//    public static final List<String> ALLOWED_ORIGINS = Collections.singletonList("https://i11a208.p.ssafy.io");
+    public static final List<String> ALLOWED_ORIGINS = Collections.singletonList("*");
     public static final List<String> ALLOWED_METHODS = Collections.singletonList("*");
     public static final List<String> ALLOWED_HEADERS = Collections.singletonList("*");
     public static final List<String> EXPOSED_HEADERS = Collections.singletonList("access");
