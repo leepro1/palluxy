d9ec487242e682178e9b484bf9003b1f76a82bf6
leebokyeong <qhrud7007@naver.com>
2024-08-13 09:58:17 +0900

    Fix: claude max token 조절
    Former-commit-id: e0fb803f033fdd559b80b92c1a0281f05d401bc5

--- backend/src/main/java/com/palluxy/domain/letter/dto/claude/ClaudeRequest.java
+++ backend/src/main/java/com/palluxy/domain/letter/dto/claude/ClaudeRequest.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 public class ClaudeRequest extends AIRequest {
 
     private final String model = "claude-3-5-sonnet-20240620";
-    private final int maxTokens = 512;
+    private final int maxTokens = 700;
     @Setter
     private List<ClaudeMessageInput> messageInputs;
 }
