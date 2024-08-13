8103d8d8626ab0bf0be3e4abf5279106af9d7498
leebokyeong <qhrud7007@naver.com>
2024-08-13 10:42:12 +0900

    Fix: session controller rollback
    Former-commit-id: 08f58dd172a6cfd477a92033b6b47d807596d297

--- backend/src/main/java/com/palluxy/domain/group/controller/SessionController.java
+++ backend/src/main/java/com/palluxy/domain/group/controller/SessionController.java
@@ -37,8 +37,6 @@ public class SessionController {
         groupHistoryService.createHistory(
             new GroupHistory(group.getLeader(), group, Action.CREATE));
 
-        openviduService.createConnection(session, null);
-
         return CommonResponse.ok(
             "Session successfully created and sessionId ready to be used", session.getSessionId());
     }
